8839b3d7e0d0a9e9d334ffbc7f983fe85626182c
almajd3713 <almajd3713@gmail.com>
2024-10-20 23:26:41 +0100

    few more fixes to test

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from api import api
 from api.routes import api_blueprint
 
-if __name__ == "__main__":
-  api.register_blueprint(api_blueprint)
-  api.run(debug=True)
+api.register_blueprint(api_blueprint)
+# api.run(debug=True)
