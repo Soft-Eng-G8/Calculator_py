5f71863495866ba0bfe5938b1e0287b88672dcfe
almajd3713 <almajd3713@gmail.com>
2024-10-20 23:31:13 +0100

    fix more...gah

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from api import api
+from api import api as app
 from api.routes import api_blueprint
 
-api.register_blueprint(api_blueprint)
+app.register_blueprint(api_blueprint)
 # api.run(debug=True)
