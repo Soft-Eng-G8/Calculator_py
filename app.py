dde78a5bf29840008df8f43ce89238b3b97826bb
almajd3713 <almajd3713@gmail.com>
2024-10-21 12:24:58 +0100

    reverting some stuff

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from api import api as app
 from api.routes import api_blueprint
 
-if __name__ == "__main__":
-  app.register_blueprint(api_blueprint)
-  app.run(debug=True)
+app.register_blueprint(api_blueprint)
